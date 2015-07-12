2f739cc229db2e7673651f109f8a4d1e9666f487
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-12 10:58:13 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -11,11 +11,11 @@ Package.onUse(function(api) {
 	
   api.versionsFrom('1.1');
 
-  api.use('constellation:console@1.0.0');
-  api.use('babrahams:temple@0.1.0');
-  api.use('constellation:subscriptions@0.1.0');
-  api.use('constellation:session@0.1.0');
-  api.use('constellation:autopublish@0.1.0');
+  api.use('constellation:console@1.0.1');
+  api.use('babrahams:temple@0.1.1');
+  api.use('constellation:subscriptions@0.1.1');
+  api.use('constellation:session@0.1.1');
+  api.use('constellation:autopublish@0.1.1');
   
 });
 
