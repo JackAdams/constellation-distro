56f8f32dce09f9dddade25ec71835e6fd6f211de
Brent Abrahams <brent_abrahams@yahoo.com>
2015-08-01 20:32:33 +0800

    Added lai:ddp-inspector
    Was worried about conflicting dependency on older version of
    Constellation, but seems to go for the latest version anyway

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:constellation',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'A curated set of packages for Constellation',
   git: 'https://github.com/JackAdams/constellation-distro.git',
   documentation: 'README.md',
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
   api.use('constellation:autopublish@0.3.2');
   api.use('constellation:tiny@0.3.1');
   api.use('constellation:position@0.3.2');
-  // api.use('lai:ddp-inspector@1.1.6');
+  api.use('lai:ddp-inspector@1.1.5');
   
   api.imply('constellation:console');
   
