b71ef4b9c19007e1cfc310fc41e4f1875f4eb122
Brent Abrahams <brent_abrahams@yahoo.com>
2015-09-03 08:32:50 +0800

    Updated babrahams:temple to latest version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:constellation',
-  version: '0.1.5',
+  version: '0.1.6',
   summary: 'A curated set of packages for Constellation',
   git: 'https://github.com/JackAdams/constellation-distro.git',
   documentation: 'README.md',
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1');
 
   api.use('constellation:console@1.2.1');
-  api.use('babrahams:temple@0.3.1');
+  api.use('babrahams:temple@0.3.2');
   api.use('constellation:subscriptions@0.3.1');
   api.use('constellation:session@0.3.2');
   api.use('constellation:autopublish@0.3.4');
