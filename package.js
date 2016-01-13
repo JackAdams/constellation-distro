39e4f0b3c00867f3bd0a0296ae2be97933cb2239
Brent Abrahams <brent_abrahams@yahoo.com>
2016-01-13 11:19:15 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -12,13 +12,13 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   api.use('constellation:console@1.4.0');
-  api.use('babrahams:temple@0.4.0');
+  api.use('babrahams:temple@0.4.1');
   api.use('constellation:subscriptions@0.4.0');
   api.use('constellation:session@0.4.0');
   api.use('constellation:autopublish@0.4.0');
   api.use('constellation:tiny@0.4.0');
   api.use('constellation:position@0.4.0');
-  api.use('lai:ddp-inspector@1.1.7');
+  api.use('lai:ddp-inspector@1.1.9');
   api.use('constellation:plugins@0.4.0');
   
   api.imply('constellation:console');
