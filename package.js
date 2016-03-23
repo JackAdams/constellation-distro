dae82eac8d675f533d353e943ccd0a3f97078fb9
Brent Abrahams <brent_abrahams@yahoo.com>
2016-03-23 22:55:09 +0800

    Updated dependency on constellation:session

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:constellation',
-  version: '0.4.2',
+  version: '0.4.3',
   summary: 'A curated set of packages for Constellation',
   git: 'https://github.com/JackAdams/constellation-distro.git',
   documentation: 'README.md',
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.use('constellation:console@1.4.0');
   api.use('babrahams:temple@0.4.1');
   api.use('constellation:subscriptions@0.4.1');
-  api.use('constellation:session@0.4.0');
+  api.use('constellation:session@0.4.1');
   api.use('constellation:autopublish@0.4.0');
   api.use('constellation:tiny@0.4.0');
   api.use('constellation:position@0.4.0');
