74b42957f2281976a1c457c80f473dba4149f837
Brent Abrahams <brent_abrahams@yahoo.com>
2016-04-07 21:48:45 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:constellation',
-  version: '0.4.4',
+  version: '0.4.5',
   summary: 'A curated set of packages for Constellation',
   git: 'https://github.com/JackAdams/constellation-distro.git',
   documentation: 'README.md',
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
   api.use('constellation:tiny@0.4.0');
   api.use('constellation:position@0.4.0');
   api.use('lai:ddp-inspector@1.1.9');
-  api.use('constellation:plugins@0.4.1');
+  api.use('constellation:plugins@0.4.2');
   
   api.imply('constellation:console');
   
